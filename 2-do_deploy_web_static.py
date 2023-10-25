810116b73224667484f643b54af84c2e1b0dc849
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-10-25 03:16:15 -0700

    Improved Task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ using the function do_deploy
 """
 
 
-from fabric.api import local, put, run, env
+from fabric.api import *
 from os.path import exists
 
 
@@ -13,29 +13,40 @@ env.hosts = ['54.90.56.157', '100.25.192.254']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to my web servers"""
+    """
+    Distributes an archive to the web servers.
+    """
     if not exists(archive_path):
+        return
+
+    filename = archive_path.split('/')[-1]
+    # Extract the filename from the full archive path
+
+    release_folder = '/data/web_static/releases/{}'.format(filename.split('.')[0])
+    # Create the release folder path
+
+    # Upload the archive to the /tmp/ directory on the web server
+    if put(archive_path, "/tmp/").failed:
         return False
 
-    try:
-        # Upload the archive to /tmp/ on the remote server
-        put(archive_path, '/tmp')
-
-        # Extract the archive to a new folder in /data/web_static/releases
-        archive_filename = archive_path.split('/')[-1]
-        release_path = f"/data/web_static/releases/{archive_filename[:-4]}"
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_path))
-
-        # Remove the archive from the remote server
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Create or update the symbolic link to the new version
-        current_path = "/data/web_static/current"
-        run("rm -f {}".format(current_path))
-        run("ln -s {} {}".format(release_path, current_path))
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
+    # Uncompress the archive to the release folder on the web server
+    if run("mkdir -p {}/".format(release_folder)).failed:
         return False
+    if run("tar -xzf /tmp/{filename} -C {release_folder}/").failed:
+        return False
+
+    # Cleanup - remove the archive and move files
+    if run("rm /tmp/{}".format(filename)).failed:
+        return False
+    if run("mv {release_folder}/web_static/* {release_folder}/").failed:
+        return False
+    if run("rm -rf {}/web_static".format(release_folder)).failed:
+        return False
+
+    # Update the symbolic link
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run(f"ln -s {release_folder} /data/web_static/current").failed:
+        return False
+
+    return True
